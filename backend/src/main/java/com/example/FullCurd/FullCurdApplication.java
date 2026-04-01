e4c8b978a84f5e2d163393cd64a0ed161b906f60
sai kumar rangineni <saikumarrangineni129@gmail.com>
2026-04-01 09:03:46 +0530

    Updated gitignore for backend

---
+++ backend/src/main/java/com/example/FullCurd/FullCurdApplication.java
@@ -0,0 +1,13 @@
+package com.example.FullCurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullCurdApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/FullCurd/FullCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FullCurd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
